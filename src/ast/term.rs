1303407e7bea360f21a79290f2ecc816019354a9
Didier Plaindoux <d.plaindoux@free.fr>
2019-01-19 18:57:26 +0100

    Add Process + format

--- src/ast/term.rs
+++ src/ast/term.rs
@@ -15,43 +15,58 @@ M ::=               -- Capability
     M.M′            -- path
 */
 
+pub trait Capability {}
+
 #[derive(Debug)]
-struct Variable<S> where S: Into<String> {
-    value: S
+pub struct Variable<S>
+where
+    S: Into<String>,
+{
+    pub value: S,
 }
 
 #[derive(Debug)]
-struct Name<S> where S: Into<String> {
-    value: S
+pub struct Name<S>
+where
+    S: Into<String>,
+{
+    pub value: S,
 }
 
 #[derive(Debug)]
-struct In<M: Capability> {
-    capability: M
+pub struct In<M: Capability> {
+    pub capability: M,
 }
 
 #[derive(Debug)]
-struct Out<M> where M: Capability {
-    capability: M
+pub struct Out<M>
+where
+    M: Capability,
+{
+    pub capability: M,
 }
 
 #[derive(Debug)]
-struct Open<M> where M: Capability {
-    capability: M
+pub struct Open<M>
+where
+    M: Capability,
+{
+    pub capability: M,
 }
 
 #[derive(Debug)]
-struct Path<N, M>
-    where N: Capability, M: Capability {
-    capability_l: N,
-    capability_r: M,
+pub struct Path<N, M>
+where
+    N: Capability,
+    M: Capability,
+{
+    pub capability_l: N,
+    pub capability_r: M,
 }
 
-struct Empty;
+pub struct Empty;
 
 // Kind of Capability
-
-pub trait Capability {}
 
 impl<S> Capability for Variable<S> where S: Into<String> {}
 
@@ -63,6 +78,143 @@ impl<M> Capability for Out<M> where M: Capability {}
 
 impl<M> Capability for Open<M> where M: Capability {}
 
-impl<N, M> Capability for Path<N, M> where N: Capability, M: Capability {}
+impl<N, M> Capability for Path<N, M>
+where
+    N: Capability,
+    M: Capability,
+{
+}
 
 impl Capability for Empty {}
+
+//
+// Processes
+//
+
+/*
+P,Q ::=
+    (νn)P           -- restriction
+    0               -- inactivity
+    P|Q             -- composition
+    !P              -- replication
+    M[P]            -- ambient
+    M.P             -- capability action
+    (x).P           -- input action
+    <M>             -- async output action
+*/
+
+pub trait Process {}
+
+#[derive(Debug)]
+pub struct Restriction<S, P>
+where
+    S: Into<String>,
+    P: Process,
+{
+    pub name: S,
+    pub process: P,
+}
+
+#[derive(Debug)]
+pub struct Inactivity;
+
+#[derive(Debug)]
+pub struct Composition<P, Q>
+where
+    P: Process,
+    Q: Process,
+{
+    pub process_l: P,
+    pub process_r: Q,
+}
+
+#[derive(Debug)]
+pub struct Replication<P>
+where
+    P: Process,
+{
+    pub process: P,
+}
+
+#[derive(Debug)]
+pub struct Ambient<M, P>
+where
+    M: Capability,
+    P: Process,
+{
+    pub capability: M,
+    pub process: P,
+}
+
+#[derive(Debug)]
+pub struct Action<M, P>
+where
+    M: Capability,
+    P: Process,
+{
+    pub capability: M,
+    pub process: P,
+}
+
+#[derive(Debug)]
+pub struct Input<X, P>
+where
+    X: Into<String>,
+    P: Process,
+{
+    pub variable: X,
+    pub process: P,
+}
+
+#[derive(Debug)]
+pub struct Output<M>
+where
+    M: Capability,
+{
+    pub message: M,
+}
+
+// Kind of process
+
+impl<N, P> Process for Restriction<N, P>
+where
+    N: Into<String>,
+    P: Process,
+{
+}
+
+impl Process for Inactivity {}
+
+impl<P, Q> Process for Composition<P, Q>
+where
+    P: Process,
+    Q: Process,
+{
+}
+
+impl<P> Process for Replication<P> where P: Process {}
+
+impl<M, P> Process for Ambient<M, P>
+where
+    M: Capability,
+    P: Process,
+{
+}
+
+impl<M, P> Process for Action<M, P>
+where
+    M: Capability,
+    P: Process,
+{
+}
+
+impl<X, P> Process for Input<X, P>
+where
+    X: Into<String>,
+    P: Process,
+{
+}
+
+impl<M> Process for Output<M> where M: Capability {}
+
+// EOF
