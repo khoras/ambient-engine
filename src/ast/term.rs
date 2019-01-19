ec83abd491c0157c8e7b76107f77eef006a4d201
Didier Plaindoux <d.plaindoux@free.fr>
2019-01-19 08:56:57 +0100

    Review Capabilities definitions

--- src/ast/term.rs
+++ src/ast/term.rs
@@ -1,45 +1,66 @@
-mod capability {
-    /*
-    M ::=               -- Capability
-        in n            -- enter
-        out n           -- exit
-        open n          -- open
-    */
+//
+// Capabilities
+//
 
-    trait Name: Into<String> {}
+/*
+M ::=               -- Capability
+    x               -- variable
+    n               -- name
+    in M            -- enter
+    out M           -- exit
+    open M          -- open
+    ε               -- empty
+    M.M′            -- path
+*/
 
-    #[derive(Debug)]
-    struct In<M: Name> {
-        m: M,
-    }
-
-    #[derive(Debug)]
-    struct Out<M: Name> {
-        m: M,
-    }
-
-    #[derive(Debug)]
-    struct Open<M: Name> {
-        m: M,
-    }
-
-    struct Empty;
-
-    // Capability trait implementation
-
-    pub trait Capability {}
-
-    impl<M: Name> Capability for In<M> {}
-
-    impl<M: Name> Capability for Out<M> {}
-
-    impl<M: Name> Capability for Open<M> {}
-
-
+#[derive(Debug)]
+struct Variable<S> where S: Into<String> {
+    value: S
 }
 
-mod process {
-
-
-
+#[derive(Debug)]
+struct Name<S> where S: Into<String> {
+    value: S
 }
+
+#[derive(Debug)]
+struct Enter<M: Capability> {
+    capability: M
+}
+
+#[derive(Debug)]
+struct Exit<M> where M: Capability {
+    capability: M
+}
+
+#[derive(Debug)]
+struct Open<M> where M: Capability {
+    capability: M
+}
+
+#[derive(Debug)]
+struct Path<N, M>
+    where N: Capability, M: Capability {
+    capability_l: N,
+    capability_r: M,
+}
+
+struct Empty;
+
+// Kind of Capability
+
+pub trait Capability {}
+
+impl<S> Capability for Variable<S> where S: Into<String> {}
+
+impl<S> Capability for Name<S> where S: Into<String> {}
+
+impl<M> Capability for Enter<M> where M: Capability {}
+
+impl<M> Capability for Exit<M> where M: Capability {}
+
+impl<M> Capability for Open<M> where M: Capability {}
+
+impl<N, M> Capability for Path<N, M> where N: Capability, M: Capability {}
+
+impl Capability for Empty {}
